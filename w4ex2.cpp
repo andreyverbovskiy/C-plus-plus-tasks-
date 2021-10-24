78cbfa583e17ff26db1fc3e2936490884f765e30
andreyverbovskiy <56063258+andreyverbovskiy@users.noreply.github.com>
2021-10-24 18:19:20 +0300

    Add files via upload

---
+++ w4ex2.cpp
@@ -0,0 +1,303 @@
+/*
+===============================================================================
+ Name        : main.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+ */
+
+#if defined (__USE_LPCOPEN)
+#if defined(NO_BOARD_LIB)
+#include "chip.h"
+#else
+#include "board.h"
+#endif
+#endif
+
+static I2CM_XFER_T i2cmXferRec;
+
+#define I2C_CLK_DIVIDER (1440) 
+
+#define I2C_BITRATE (50000)
+
+#define I2C_MODE (0)
+
+#define I2C_TEMP_ADDR (0x4A)
+
+#define TICKRATE_HZ (1000)
+
+#define STANDBY  (1 << 7)
+
+#define ARRAYSIZE (80)
+
+#include <stdio.h>
+#include <cr_section_macros.h>
+
+
+static volatile int counter;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/**
+ * @brief Handle interrupt from SysTick timer
+ * @return Nothing
+ */
+
+void SysTick_Handler(void)
+{
+	if(counter > 0) counter--;
+}
+
+#ifdef __cplusplus
+}
+
+#endif
+void Sleep(int ms)
+{
+	counter = ms;
+	while(counter > 0) {
+		__WFI();
+	}
+}
+
+
+class DigitalIoPin {
+public:
+	DigitalIoPin(int port, int pin, bool input = true, bool pullup = true, bool invert = false);
+	DigitalIoPin(const DigitalIoPin &) = delete;
+	virtual ~DigitalIoPin() {};
+	bool read();
+	void write(bool value);
+private:
+	int port1, pin1;
+	uint32_t modefunc;
+};
+
+
+
+DigitalIoPin::DigitalIoPin(int port, int pin, bool input, bool pullup, bool invert): port1(port), pin1(pin){
+
+	if(pullup == true){
+		if(invert == true){
+			modefunc = (IOCON_MODE_PULLUP | IOCON_DIGMODE_EN);
+		}else{
+			modefunc = (IOCON_MODE_PULLUP | IOCON_DIGMODE_EN | IOCON_INV_EN);
+		}
+	}else{
+
+		if(invert == true){
+			modefunc = (IOCON_MODE_INACT | IOCON_DIGMODE_EN );
+
+		}else{
+			modefunc = (IOCON_MODE_INACT | IOCON_DIGMODE_EN | IOCON_INV_EN);
+		}
+	}
+
+	Chip_IOCON_PinMuxSet(LPC_IOCON, port1, pin1, modefunc);
+
+	if(input == true){
+		Chip_GPIO_SetPinDIRInput(LPC_GPIO, port1, pin1);
+	}else{
+		Chip_GPIO_SetPinDIROutput(LPC_GPIO, port1, pin1);
+	}
+
+
+}
+
+bool DigitalIoPin::read(){
+	return Chip_GPIO_GetPinState(LPC_GPIO, port1, pin1);
+
+}
+
+void DigitalIoPin::write(bool state){
+	Chip_GPIO_SetPinState(LPC_GPIO, port1, pin1, !state);
+}
+
+
+static bool ReadConfigI2CM(void);
+
+static void Init_I2C_PinMux(void){
+	Chip_IOCON_PinMuxSet(LPC_IOCON, 0, 22, IOCON_DIGMODE_EN | I2C_MODE);
+	Chip_IOCON_PinMuxSet(LPC_IOCON, 0, 23, IOCON_DIGMODE_EN | I2C_MODE);
+	Chip_SWM_EnableFixedPin(SWM_FIXED_I2C0_SCL);
+	Chip_SWM_EnableFixedPin(SWM_FIXED_I2C0_SDA);
+}
+
+static void setupI2CMaster(){
+	Chip_I2C_Init(LPC_I2C0);
+	Chip_I2C_SetClockDiv(LPC_I2C0, I2C_CLK_DIVIDER);
+	Chip_I2CM_SetBusSpeed(LPC_I2C0, I2C_BITRATE);
+	Chip_I2CM_Enable(LPC_I2C0);
+}
+
+static void SetupXferRecAndExecute(uint8_t devAddr,
+		uint8_t *txBuffPtr,
+		uint16_t txSize,
+		uint8_t *rxBuffPtr,
+		uint16_t rxSize)
+{
+	i2cmXferRec.slaveAddr = devAddr;
+	i2cmXferRec.status = 0;
+	i2cmXferRec.txSz = txSize;
+	i2cmXferRec.rxSz = rxSize;
+	i2cmXferRec.txBuff = txBuffPtr;
+	i2cmXferRec.rxBuff = rxBuffPtr;
+
+	Chip_I2CM_XferBlocking(LPC_I2C0, &i2cmXferRec);
+}
+
+static void ReadTemperatureI2CM(void){
+	uint8_t temperature[1];
+	uint8_t tc74TempRegisterAddress = 0;
+
+	if (ReadConfigI2CM()) {
+		if(i2cmXferRec.status == I2CM_STATUS_OK){
+			SetupXferRecAndExecute(
+					I2C_TEMP_ADDR,
+					&tc74TempRegisterAddress,
+					1,
+					temperature,
+					1
+			);
+			Board_UARTPutSTR("\r\nCurrent temperature is: \r\n");
+			char c[10];
+			sprintf(c, "%d", (int8_t)temperature[0]);
+			Board_UARTPutSTR(c);
+		}
+	}
+	else {
+		Board_UARTPutSTR("\r\nNo data yet.\r\n");
+	}
+}
+
+static bool ReadConfigI2CM(void) {
+	uint8_t tc74ConfigRegisterAddress = 1; // 0x01 for config
+	uint8_t config[1];
+
+	SetupXferRecAndExecute(
+			I2C_TEMP_ADDR,
+			&tc74ConfigRegisterAddress,
+			1,
+			config,
+			1
+	);
+
+	if(i2cmXferRec.status == I2CM_STATUS_OK){
+		if(((config[0] >> 6) & 1U )== 1){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	return false;
+}
+
+static bool ReadStandbyI2CM(void) {
+	uint8_t tc74ConfigRegisterAddress = 1; // 0x01 for config
+	uint8_t config[1];
+
+	SetupXferRecAndExecute(
+			I2C_TEMP_ADDR,
+			&tc74ConfigRegisterAddress,
+			1,
+			config,
+			1
+	);
+
+	if (i2cmXferRec.status == I2CM_STATUS_OK) {
+		if(((config[0] >> 7) & 1U )== 1){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	return false;
+}
+
+
+void Set_SysTickRate(int frequency){
+	uint32_t sysTickRate;
+	Chip_Clock_SetSysTickClockDiv(1);
+	sysTickRate = Chip_Clock_GetSysTickClockRate();
+	SysTick_Config(sysTickRate / frequency);
+}
+
+
+int main(void) {
+
+#if defined (__USE_LPCOPEN)
+	// Read clock settings and update SystemCoreClock variable
+	SystemCoreClockUpdate();
+#if !defined(NO_BOARD_LIB)
+	// Set up and initialize all required blocks and
+	// functions related to the board hardware
+	Board_Init();
+	
+#endif
+#endif
+	
+	CoreDebug->DEMCR |= 1 << 24;
+	DWT->CTRL |= 1;
+
+	Init_I2C_PinMux();
+	setupI2CMaster();
+	NVIC_DisableIRQ(I2C0_IRQn);
+	Set_SysTickRate(TICKRATE_HZ);
+	DigitalIoPin sw3(1,9,true,true,true);
+	Sleep(100);
+	int timer;
+	uint32_t timestamp;
+	int sample = 1;
+
+	set_normal();
+
+	while(1){
+		if(timer >= 5000){
+			timer = 0;
+			if(!ReadStandbyI2CM()){
+				set_Standby();
+			}
+		}
+
+		if(sw3.read()){
+			timer = 0;
+			if(ReadStandbyI2CM()){
+				set_norm();
+			}
+			timestamp = (uint32_t)DWT->CYCCNT / 72000;
+			uint8_t temperature = ReadTemperatureI2CM();
+			print_temp(sample, timestamp, temperature);
+			sample++;
+		}
+		Sleep(1);
+		timer++;
+	}
+	return 0 ;
+}
+
+void set_Standby(){
+	uint8_t tc74ConfigRegisterAddress = 1;
+	// to write to the register, two bytes back to back have to be sent
+	uint8_t arr[2] = {tc74ConfigRegisterAddress,STANDBY};
+	SetupXferRecAndExecute(
+			I2C_TEMP_ADDR,
+			arr, 2,
+			NULL,0); // empty
+}
+void set_norm(){
+	uint8_t tc74ConfigRegisterAddress = 1;
+	uint8_t arr[2] = {tc74ConfigRegisterAddress,0};
+	SetupXferRecAndExecute(
+			I2C_TEMP_ADDR,
+			arr, 2,
+			NULL,0); // empty
+}
+
+void print_temp(int sample,uint32_t timestamp, uint8_t temperature){
+	char buffer[ARRAYSIZE];
+	snprintf(buffer,ARRAYSIZE,"\n\r{\"samplenr\": %d,\"timestamp\": %u,\"temperature\":%u\n\r}",sample,timestamp,temperature);
+	Board_UARTPutSTR(buffer);
+}
